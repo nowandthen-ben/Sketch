c087fb324eca71674b76ee796c410bbf134c0ba1
nowandthen-ben <137456122+nowandthen-ben@users.noreply.github.com>
2023-07-30 18:43:51 -0400

    Add a button to set custom grid size.

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
+gridInit(16);
+
+function gridInit(size) {
+    const spacer = document.querySelector('.spacer');
+    spacer.textContent = '';
+    for (let i = 0; i < size; i++) {
+        pixelRow = document.createElement('div');
+        pixelRow.classList.add('pixelRow');
+        spacer.appendChild(pixelRow);
+        for (let j = 0; j < size; j++) {
+            pixel = document.createElement('div');
+            pixel.classList.add('pixel');
+            pixel.addEventListener('mouseenter', colorize)
+            pixelRow.appendChild(pixel);
+        }
+    }
+}
+
 function colorize(e) {
     e.target.classList.add('darkened');
 }
 
-for (let i = 0; i < 16; i++) {
-    const spacer = document.querySelector('.spacer');
-    pixelRow = document.createElement('div');
-    pixelRow.classList.add('pixelRow');
-    spacer.appendChild(pixelRow);
-    for (let j = 0; j < 16; j++) {
-        pixel = document.createElement('div');
-        pixel.classList.add('pixel');
-        pixel.addEventListener('mouseenter', colorize)
-        pixelRow.appendChild(pixel);
-    }
-}
+const resetBtn = document.querySelector('button');
+resetBtn.addEventListener('click', reset);
 
+function reset() {
+    x = prompt('Set grid to what size? (1-40)');
+    while (x < 1 || x > 40 || isNaN(x) == true) {
+        if (x === null) {
+            return;
+        }
+        alert('Invalid input, try again.');
+        x = prompt('Set grid to what size? (1-40)');
+    }
+    gridInit(x);
+}
